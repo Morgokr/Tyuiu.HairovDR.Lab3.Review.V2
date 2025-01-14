2a729f3c4cd43cbecd8e5b95e346542bdc18f9e2
Morgokr <127106532+Morgokr@users.noreply.github.com>
2025-01-15 02:31:41 +0500

    Add files via upload

---
+++ SolveMathSecondExpression.Lib/MathSecondExpression.cs
@@ -0,0 +1,15 @@
+﻿namespace SolveMathSecondExpression.Lib
+{
+    public static class MathSecondExpression
+    {
+        public static float CalculateSecondExpression(float arg1, float arg2, float arg3)
+        {
+            var a = (float)arg1;
+            var x = (float)arg2;
+            var y = (float)arg3;
+
+            return 6 * x + ((2 * x + y - 3 * a) / (a + 2 * x - 7 * y)) + ( ((x+3) / y) / ((2 - x) / x));
+        }
+
+    }
+}
