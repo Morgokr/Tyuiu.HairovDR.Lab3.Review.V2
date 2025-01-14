b5ffe496d540fc3f09478d8247e2bb12ed9811f9
Morgokr <127106532+Morgokr@users.noreply.github.com>
2025-01-15 02:30:49 +0500

    Add files via upload

---
+++ SolveMathFirstExpression.Lib/MathFirstExpression.cs
@@ -0,0 +1,13 @@
+﻿namespace SolveMathFirstExpression.Lib
+{
+    public static class MathFirstExpression
+    {
+        public static float CalculateFirstExpression(float arg1, float arg2, float arg3)
+        {
+            var a = (float)arg1;
+            var x = (float)arg2;
+            var y = (float)arg3;
+            return 12 * y - ((3 * x + 2 * a + a) / (3 * a + 2 * x - y)) + 12 * y + ((x / y + 2 + a / y) / ((2 - a + 1 ) / x) );
+        }
+    }
+}
